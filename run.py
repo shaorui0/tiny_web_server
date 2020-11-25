0fbe33ce6a6912736f283863d668f2d158d2aea1
shaorui0 <sr1054461216@163.com>
2020-11-25 16:19:16 +0800

    can server static content

--- run.py
+++ run.py
@@ -1,17 +1,21 @@
+import os
 import socket
 import typing
+import mimetypes
 
 HOST = "127.0.0.1"
 PORT = 9000
+SERVER_ROOT = 'www'
 
-# response will be parsed by browser and print content of body(html).
+# request status code
+
 # 200
-RESPONSE = b"""\
+FILE_RESPONSE_TEMPLATE = """\
 HTTP/1.1 200 OK
-Content-type: text/html
-Content-length: 15
+Content-type: {content_type}
+Content-length: {content_length}
 
-<h1>Hello!</h1>""".replace(b"\n", b"\r\n")
+""".replace("\n", "\r\n")
 
 # 400
 BAD_REQUEST_RESPONSE = b"""\
@@ -21,6 +25,21 @@ Content-length: 11
 
 Bad Request""".replace(b"\n", b"\r\n")
 
+# 404, cannot find index file
+NOT_FOUND_RESPONSE = b"""\
+HTTP/1.1 404 Not Found
+Content-type: text/plain
+Content-length: 9
+
+Not Found""".replace(b"\n", b"\r\n")
+
+# 405, Method Not Allowed
+METHOD_NOT_ALLOWED_RESPONSE = b"""\
+HTTP/1.1 405 Method Not Allowed
+Content-type: text/plain
+Content-length: 17
+
+Method Not Allowed""".replace(b"\n", b"\r\n")
 
 def iter_lines(a_socket, buf_size=16_384):
     buf = b""
@@ -59,6 +78,37 @@ def iter_lines(sock: socket.socket, bufsize: int = 16_384) -> typing.Generator[b
                 yield line
             except IndexError:
                 break
+
+
+def server_static(path, sock):
+    if path == '/':
+        path = '/index.html'
+
+    abspath = os.path.normpath(os.path.join(SERVER_ROOT, path.lstrip("/")))
+    if not abspath.startswith(SERVER_ROOT):
+        sock.sendall(NOT_FOUND_RESPONSE)
+        return
+        
+    try:
+        with open(abspath, 'rb') as f:
+            stat = os.fstat(f.fileno())
+            content_type, encoding = mimetypes.guess_type(abspath)
+
+            if content_type is None:
+                content_type = "application/octet-stream"
+            if encoding is not None:
+                content_type += f"; charset={encoding}"
+
+            response_headers = FILE_RESPONSE_TEMPLATE.format(
+                content_type=content_type,
+                content_length=stat.st_size,
+            ).encode("ascii")
+
+            sock.sendall(response_headers)
+            sock.sendfile(f)
+    except FileNotFoundError:
+        sock.sendall(NOT_FOUND_RESPONSE)
+        return
 
 class Request(typing.NamedTuple): # TODO
     method: str
@@ -100,7 +150,12 @@ with socket.socket() as server_sock:
             try: # Try not to crash the server
                 request = Request.from_socket(client_sock)
                 print(request)
-                client_sock.sendall(RESPONSE)
+
+                if request.method != "GET":
+                    client_sock.sendall(METHOD_NOT_ALLOWED_RESPONSE)
+                    continue
+                
+                server_static(request.path, client_sock)
             except Exception as e:
                 print(f"Failed to parse request: {e}")
                 client_sock.sendall(BAD_REQUEST_RESPONSE)
