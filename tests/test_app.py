1818d524da68ae18973e9893c28ee90c20e4d12c
shaorui0 <sr1054461216@163.com>
2020-12-06 21:22:17 +0800

    test: application, smoke test

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,11 +10,11 @@ app = Application()
 
 @app.route("/")
 def static_handler(request):
-    return Response("200", content="static")
+    return Response("200 OK", content="static")
 
 @app.route("/people/{name}/{age}")
 def dynamic_handler(request, name, age):
-    return Response("200", content=f"{name} is {age} years old!")
+    return Response("200 OK", content=f"{name} is {age} years old!")
 
 
 class ApplicationTesting(unittest.TestCase):
@@ -25,7 +25,11 @@ class ApplicationTesting(unittest.TestCase):
     def test_application_can_run_dynamic_request(self):
         response = app(Request(method="GET", path="/people/Justin/24", headers=Headers(), body=BytesIO()))
         self.assertEqual(response.body.read(), b"Justin is 24 years old!")
-        print(app.router.routes_by_method)
-        
+
+    def test_applications_can_fail_to_route_invalid_paths(self):
+        response = app(Request(method="GET", path="/fail", headers=Headers(), body=BytesIO()))
+        self.assertEqual(response.body.read(), b"Not Found")
+    
+    
 if __name__ == '__main__':
     unittest.main()
