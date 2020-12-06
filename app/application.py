8c5e5641d218110f7d759a996c4404bdda16e1e4
shaorui0 <sr1054461216@163.com>
2020-12-06 21:00:46 +0800

    test: application, static content

--- app/application.py
+++ app/application.py
@@ -19,11 +19,7 @@ class Application:
         method = "GET",
         name = None,
     ):
-        # 如何使用？参数是requst，返回是response
-        # 这个函数的目的是add router
         def decorator(handler):
-            # 输入是什么？
-            # 输出是什么？如何对内部的函数进行“升级”？直接不升级，只是做add的操作
             self.add_router(path, method, name, handler)
             return handler
         return decorator
