17bfddf54ab3c7a9dc86b53ae32a2003e67dc455
shaorui0 <sr1054461216@163.com>
2020-12-06 20:59:17 +0800

    class appication: append add_router method

--- app/application.py
+++ app/application.py
@@ -1,20 +1,38 @@
-from router import Router
-from request import Request
-from response import Response
+from .router import Router
+from .request import Request
+from .response import Response
 
 class Application:
-    self.router = Router()
+    def __init__(self):
+        self.router = Router()
 
-    def route(
-        self,
-        path, 
-        method = "GET",
-        name = None,
-    ):
-        pass
     
     def __call__(self, requst):
         handler = self.router.lookup(requst.method, requst.path)
         if handler is None:
             return Response("404 Not Found", content="Not Found")
         return handler(requst)
+    
+    def route(
+        self,
+        path, 
+        method = "GET",
+        name = None,
+    ):
+        # 如何使用？参数是requst，返回是response
+        # 这个函数的目的是add router
+        def decorator(handler):
+            # 输入是什么？
+            # 输出是什么？如何对内部的函数进行“升级”？直接不升级，只是做add的操作
+            self.add_router(path, method, name, handler)
+            return handler
+        return decorator
+    
+    def add_router(
+        self,
+        path, 
+        method = "GET",
+        name = None,
+        handler,
+    ):
+        self.router.add_router(name or handler.__name__, method, path, handler)
