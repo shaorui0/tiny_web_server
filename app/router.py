a3eae4b52cf0b3568041c03a48c42bdfd8973dd3
shaorui0 <sr1054461216@163.com>
2020-12-06 20:05:54 +0800

    append annotation of class Router

--- app/router.py
+++ app/router.py
@@ -8,9 +8,20 @@ class Router:
         self.names = set()
         
     def add_router(self, name, method, path, handler):
+        """keep a pair ((name, mothod, path): handler)
+
+        Args:
+            name (str): used to identify the function is not repeated
+            method (str): HTTP method
+            path (str): url path
+            handler (function obj): bussinese logic function object,
+
+        Raises:
+            ValueError: current router already exists.
+        """
         assert path.startswith("/"), "paths must start with '/'"
         if name in self.names:
-            raise ValueError(f"A route named {name} already exists.")
+            raise ValueError(f"A router named {name} already exists.")
         
         route_template = ""
         for segment in path.split("/")[1:]:
@@ -25,12 +36,26 @@ class Router:
         # log
         
     def lookup(self, method, path):
+        """Find handler function based on method(default='GET') and url path
+
+        Args:
+            method (str): HTTP method, default is 'GET'
+            path (str): url path
+
+        Returns:
+            handler: bussinese logic function object,
+                parameters: 
+                    request, 
+                    [option]: a dict include varable and value of varable
+                return: 
+                    response
+        """
         for route_re, handler in self.routes_by_method[method].values():
             match = route_re.match(path)
             if match is not None:
-                params = match.groupdict()
+                params = match.groupdict() # like: {"user_name": "Jack"}
                 # log
-                return functools.partial(handler, **params)
+                return functools.partial(handler, **params) # extended parameters
         # log
         return None
 
